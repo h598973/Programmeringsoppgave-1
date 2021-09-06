8ee3f6b03abdab7ef861b333a96d936ac19554a4
h598973 <598973@stud.hvl.no>
2021-09-06 16:53:56 +0200

    test

---
+++ Programmeringsoppgaver/src/oppgaver/Oppgave1.java
@@ -0,0 +1,10 @@
+package oppgaver;
+
+public class Oppgave1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Programmeringsoppgaver/src/oppgaver/Oppgave2.java
@@ -0,0 +1,10 @@
+package oppgaver;
+
+public class Oppgave2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
