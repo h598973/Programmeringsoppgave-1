3e98ecb4e905f32d820a78e78b5cf838395387ec
h598973 <598973@stud.hvl.no>
2021-09-10 19:07:14 +0200

    Oppgaver

--- Programmeringsoppgaver/src/oppgaver/Oppgave3.java
+++ Programmeringsoppgaver/src/oppgaver/Oppgave3.java
@@ -1,5 +1,19 @@
 package oppgaver;
 
+import static javax.swing.JOptionPane.showInputDialog;
+
 public class Oppgave3 {
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = Integer.parseInt(showInputDialog("Heltall?")); // setter inn heltall
+		int x = n; // gir x en midlertidig verdi
+
+		do {
+			x = x * (n - 1);
+			n--;
+
+		} while (n > 1);
+		System.out.println("n! er " + x);
+	}
 }
